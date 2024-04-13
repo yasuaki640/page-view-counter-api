37c8f26128babcb6e77af530d75b482c91aedb49
yasuaki640 <yasuaki640@gmail.com>
2024-04-14 00:45:20 +0900

    あああああああミスうううう

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,25 @@ type Bindings = {
 	DB: D1Database;
 };
 
+type IncrementRes =
+	| { success: true; count: number }
+	| { success: false };
+
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/increment-count", async (c) => {
 	const db = c.env.DB;
-	const stmt = db.prepare("INSERT INTO Accesses DEFAULT VALUES;");
+	const insertStmt = db.prepare("INSERT INTO Accesses DEFAULT VALUES;");
+	const countStmt = db.prepare("SELECT COUNT(*) AS count FROM Accesses;");
 
 	try {
-		await stmt.run();
+		await insertStmt.run();
+		const count = await countStmt.first<number>("count") ?? 0;
+		return c.json<IncrementRes>({ success: true, count }, 201);
 	} catch (e) {
-		return c.json({ success: false }, 500);
+		console.error(e);
+		return c.json<IncrementRes>({ success: false }, 500);
 	}
-
-	return c.json({ success: true }, 204);
 });
 
 export default app;
