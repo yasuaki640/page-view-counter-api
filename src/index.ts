4030b04c6f075b76247e0408e935dccf4009922a
yasuaki640 <yasuaki640@gmail.com>
2024-04-14 11:36:32 +0900

    Implement CORS and optimize count calculation
    Added the CORS middleware to enable cross-origin resource sharing, and configured it using the ALLOW_ORIGIN environment variable. Also, made minor optimization to the count selection statement in the "/increment-count" route to improve readability and consistency.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { createMiddleware } from "hono/factory";
 
 type Bindings = {
 	DB: D1Database;
+	ALLOW_ORIGIN: string;
 };
 
-type IncrementRes =
-	| { success: true; count: number }
-	| { success: false };
+type IncrementRes = { success: true; count: number } | { success: false };
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+const corsMiddleware = createMiddleware<{ Bindings: Bindings }>(
+	async (c, next) => {
+		const middleware = cors({ origin: c.env.ALLOW_ORIGIN });
+		return middleware(c, next);
+	},
+);
+app.use(corsMiddleware);
 
 app.get("/increment-count", async (c) => {
 	const db = c.env.DB;
@@ -17,7 +26,7 @@ app.get("/increment-count", async (c) => {
 
 	try {
 		await insertStmt.run();
-		const count = await countStmt.first<number>("count") ?? 0;
+		const count = (await countStmt.first<number>("count")) ?? 0;
 		return c.json<IncrementRes>({ success: true, count }, 201);
 	} catch (e) {
 		console.error(e);
