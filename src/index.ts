389b8a18acac19178510d15405562e628c93af26
yasuaki640 <yasuaki640@gmail.com>
2024-04-23 23:35:22 +0900

    close #1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { createMiddleware } from "hono/factory";
+import { z } from "zod";
 import type { Bindings, IncrementRes } from "./types";
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -13,19 +15,39 @@ const corsMiddleware = createMiddleware<{ Bindings: Bindings }>(
 );
 app.use(corsMiddleware);
 
-app.get("/increment-count", async (c) => {
-	const db = c.env.DB;
-	const insertStmt = db.prepare("INSERT INTO Accesses DEFAULT VALUES;");
-	const countStmt = db.prepare("SELECT COUNT(*) AS count FROM Accesses;");
+app.get(
+	"/increment-count",
+	zValidator("query", z.object({ visitorId: z.string() })),
+	async (c) => {
+		// biome-ignore lint: lint/style/noNonNullAssertion
+		const visitorId = c.req.query("visitorId")!;
+		const { success } = await c.env.RATE_LIMITER.limit({ key: visitorId });
 
-	try {
-		await insertStmt.run();
-		const count = (await countStmt.first<number>("count")) ?? 0;
-		return c.json<IncrementRes>({ success: true, count }, 201);
-	} catch (e) {
-		console.error(e);
-		return c.json<IncrementRes>({ success: false }, 500);
-	}
-});
+		try {
+			if (!success) {
+				const count = await selectCount(c.env.DB);
+				return c.json<IncrementRes>({ success: true, count }, 200);
+			}
+
+			await incrementCount(c.env.DB);
+			const count = await selectCount(c.env.DB);
+			return c.json<IncrementRes>({ success: true, count }, 201);
+		} catch (e) {
+			console.error(e);
+			return c.json<IncrementRes>({ success: false }, 500);
+		}
+	},
+);
+
+// Database operations
+const selectCount = async (db: Bindings["DB"]) => {
+	const countStmt = db.prepare("SELECT COUNT(*) AS count FROM Accesses;");
+	return (await countStmt.first<number>("count")) ?? 0;
+};
+
+const incrementCount = async (db: Bindings["DB"]) => {
+	const insertStmt = db.prepare("INSERT INTO Accesses DEFAULT VALUES;");
+	await insertStmt.run();
+};
 
 export default app;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,9 @@
 export type Bindings = {
 	DB: D1Database;
 	ALLOW_ORIGIN: string;
+	RATE_LIMITER: {
+		limit: (args: { key: string }) => Promise<{ success: boolean }>;
+	};
 };
 
 export type IncrementRes =
